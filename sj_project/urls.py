37c5a9ef62b9c0dbd24cc02e28697e48ad79fdab
dyoJ <sojung10203@gmail.com>
2025-04-13 17:55:38 +0900

    2주차 django 개발 완료

---
+++ sj_project/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('product/', include('product.urls')),
+    path('', include('pages.urls')),
+]
